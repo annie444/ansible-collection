230a3a53ae3b8ec751e795222c3ed6e6f5e7e8e7
Annie Ehler <annie.ehler.4@gmail.com>
2025-03-02 15:42:26 -0800

    Fix the exception classes

--- plugins/module_utils/kanidm/exceptions.py
+++ plugins/module_utils/kanidm/exceptions.py
@@ -10,40 +10,40 @@ from ansible.errors import (
 class KanidmException(AnsibleError):
     @property
     def message(self):
-        return f"Kanidm error: {self._message} ({super()._message})"
+        return f"Kanidm error: {self._message}"
 
 
 class KanidmArgsException(AnsibleOptionsError):
     @property
     def message(self):
-        return f"Kanidm argument error: {self._message} ({super()._message})"
+        return f"Kanidm argument error: {self._message}"
 
 
 class KanidmRequiredOptionError(AnsibleRequiredOptionError):
     @property
     def message(self):
-        return f"Kanidm required argument error: {self._message} ({super()._message})"
+        return f"Kanidm required argument error: {self._message}"
 
 
 class KanidmModuleError(AnsibleModuleError):
     @property
     def message(self):
-        return f"Kanidm module error: {self._message} ({super()._message})"
+        return f"Kanidm module error: {self._message}"
 
 
 class KanidmAuthenticationFailure(AnsibleAuthenticationFailure):
     @property
     def message(self):
-        return f"Kanidm authentication error: {self._message} ({super()._message})"
+        return f"Kanidm authentication error: {self._message}"
 
 
 class KanidmUnexpectedError(AnsibleError):
     @property
     def message(self):
-        return f"Kanidm unexpected error: {self._message} ({super()._message})"
+        return f"Kanidm unexpected error: {self._message}"
 
 
 class KanidmApiError(AnsibleModuleError):
     @property
     def message(self):
-        return f"Kanidm API error: {self._message} ({super()._message})"
+        return f"Kanidm API error: {self._message}"
