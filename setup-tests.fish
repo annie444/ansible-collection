ab822695b91270c9d467f9e5a8f3e6da35d702df
Annie Ehler <annie.ehler.4@gmail.com>
2025-03-01 16:16:49 -0800

    Update the release with fixed requirements

--- setup-tests.fish
+++ setup-tests.fish
@@ -2,18 +2,18 @@
 
 podman machine start || true
 
-cp ./kanidm-test-vol.tar.gz ./kanidm-test-vol.tar.gz.old || true
+cp ./tests/data/kanidm-test-vol.tar.gz ./tests/data/kanidm-test-vol.tar.gz.old || true
 
-gzip -d ./kanidm-test-vol.tar.gz >kanidm-test-vol.tar
+gzip -d ./tests/data/kanidm-test-vol.tar.gz >./tests/data/kanidm-test-vol.tar
 
 if not podman volume inspect kanidm-test-vol >/dev/null 2>&1
     podman volume create kanidm-test-vol
     sleep 1
     if test (uname -s) = Darwin
-        set -gx PODMAN_VOL_PATH (pwd)
+        set -gx PODMAN_VOL_PATH (pwd)/tests/data
         podman machine ssh (podman machine inspect --format="{{.Name}}") sudo podman volume import kanidm-test-vol $PODMAN_VOL_PATH/kanidm-test-vol.tar
     else
-        podman volume import kanidm-test-vol ./kanidm-test-vol.tar
+        podman volume import kanidm-test-vol ./tests/data/kanidm-test-vol.tar
     end
 end
 
@@ -21,5 +21,5 @@ sleep 1
 
 podman run --name=kanidm-test --publish=8443:8443 --volume=kanidm-test-vol:/data:rw --detach --rm docker.io/kanidm/server:latest
 
-rm kanidm-test-vol.tar || true
-mv ./kanidm-test-vol.tar.gz.old ./kanidm-test-vol.tar.gz || true
+rm ./tests/data/kanidm-test-vol.tar || true
+mv ./tests/data/kanidm-test-vol.tar.gz.old ./tests/data/kanidm-test-vol.tar.gz || true
