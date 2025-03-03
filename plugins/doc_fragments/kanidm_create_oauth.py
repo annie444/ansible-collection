31f8354ccb7bcd0cacf1ce82bd7ddb11695a568c
Annie Ehler <annie.ehler.4@gmail.com>
2025-03-02 17:38:33 -0800

    Add proper ansible doc fragments

--- plugins/doc_fragments/kanidm_create_oauth.py
+++ plugins/doc_fragments/kanidm_create_oauth.py
@@ -1,6 +1,6 @@
 from ..module_utils.kanidm.arg_specs import KanidmOauthArgs
 
 
-class ModuleDocFragment:
+class ModuleDocFragment(object):
     # This is the main documentation for the module which is displayed in ansible-doc.
     DOCUMENTATION = KanidmOauthArgs.documentation()
