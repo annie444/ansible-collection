b880b299e7cdc6180e08b07099e2e9af3e883926
Annie Ehler <annie.ehler.4@gmail.com>
2025-03-02 16:34:54 -0800

    Add logic to run sidecar container

--- setup-tests.sh
+++ setup-tests.sh
@@ -1,50 +1,53 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
-if not command -v podman >/dev/null 2>&1 && not command -v docker >/dev/null 2>&1
+if ! command -v podman >/dev/null 2>&1 && ! command -v docker >/dev/null 2>&1; then
     echo "Neither Podman nor Docker is installed"
     exit 1
-end
+fi
 
 cp ./tests/data/kanidm-test-vol.tar.gz ./tests/data/kanidm-test-vol.tar.gz.old || true
 
 gzip -d ./tests/data/kanidm-test-vol.tar.gz >./tests/data/kanidm-test-vol.tar
 
-if command -v podman >/dev/null 2>&1
-    if podman container top kanidm-test >/dev/null 2>&1
-        exit 0
-    end
+if command -v podman >/dev/null 2>&1; then
 
-    if test (uname -s) = Darwin
+    if podman container top kanidm-test >/dev/null 2>&1; then
+        exit 0
+    fi
+
+    if [ "$(uname -s)" = "Darwin" ]; then
         podman machine start || true
-    end
-    if not podman volume inspect kanidm-test-vol >/dev/null 2>&1
+    fi
+
+    if ! podman volume inspect kanidm-test-vol >/dev/null 2>&1; then
         podman volume create kanidm-test-vol
         sleep 1
-        if test (uname -s) = Darwin
-            set -gx PODMAN_VOL_PATH (pwd)/tests/data
-            podman machine ssh (podman machine inspect --format="{{.Name}}") sudo podman volume import kanidm-test-vol $PODMAN_VOL_PATH/kanidm-test-vol.tar
+        if [ "$(uname -s)" = "Darwin" ]; then
+            export PODMAN_VOL_PATH="$(pwd)/tests/data"
+            export PODMAN_MACHINE_NAME="$(podman machine inspect --format='{{.Name}}')"
+            podman machine ssh "${PODMAN_MACHINE_NAME}" sudo podman volume import kanidm-test-vol "${PODMAN_VOL_PATH}/kanidm-test-vol.tar"
         else
             podman volume import kanidm-test-vol ./tests/data/kanidm-test-vol.tar
-        end
-    end
+        fi
+    fi
 
     sleep 1
     podman run --name=kanidm-test --publish=8443:8443 --volume=kanidm-test-vol:/data:rw --detach --rm docker.io/kanidm/server:latest
 
 else
-    if docker container top kanidm-test >/dev/null 2>&1
+    if docker container top kanidm-test >/dev/null 2>&1; then
         exit 0
-    end
-    if not docker volume inspect kanidm-test-vol >/dev/null 2>&1
+    fi
+
+    if ! docker volume inspect kanidm-test-vol >/dev/null 2>&1; then
         docker volume create kanidm-test-vol
         sleep 1
         docker volume import kanidm-test-vol ./tests/data/kanidm-test-vol.tar
-    end
+    fi
 
     sleep 1
     docker run --name=kanidm-test --publish=8443:8443 --volume=kanidm-test-vol:/data:rw --detach --rm docker.io/kanidm/server:latest
-
-end
+fi
 
 rm ./tests/data/kanidm-test-vol.tar || true
 mv ./tests/data/kanidm-test-vol.tar.gz.old ./tests/data/kanidm-test-vol.tar.gz || true
