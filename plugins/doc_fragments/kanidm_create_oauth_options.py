358657a1733946b5a1a09ec8b13a184413bf2078
Annie Ehler <annie.ehler.4@gmail.com>
2025-03-02 19:06:37 -0800

    Fix yaml dumping

--- plugins/doc_fragments/kanidm_create_oauth_options.py
+++ plugins/doc_fragments/kanidm_create_oauth_options.py
@@ -4,5 +4,5 @@ from ..module_utils.kanidm.arg_specs import KanidmOauthArgs
 class ModuleDocFragment(object):
     # This is the main documentation for the module which is displayed in ansible-doc.
     DOCUMENTATION = r"""
-        options:
+    options:
     """ + KanidmOauthArgs.documentation(indentation=2)
